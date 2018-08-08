e5128b7a18327a9980f29ee2004d31dcc2d56c51
Bradd3rs <tsbweb@gmail.com>
2018-08-08 20:07:01 +0100

    grid created

--- app/src/App.js
+++ app/src/App.js
@@ -1,155 +1,31 @@
 import React, { Component } from 'react';
-import Player from './components/Player';
-import Masteries from './components/Masteries';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      playerLevel: 1,
-      exp: 0,
-      maxExp: 5,
-      gainedLevel: false,
-      health: 10,
-      maxHealth: 10,
-      skillPoints: 0,
-      zone: 1,
-      crawling: false,
-      damageMultiplier: 1
+      gridSize: 3,
+      grid: []
     }
-    this.crawl = this.crawl.bind(this);
-    this.crawling = this.crawling.bind(this);
-    this.reduceHealth = this.reduceHealth.bind(this);
-    this.resetHealth = this.resetHealth.bind(this);
-    this.increaseMaxHealth = this.increaseMaxHealth.bind(this);
-    this.addExp = this.addExp.bind(this);
-    this.levelUp = this.levelUp.bind(this);
-    this.increaseZone = this.increaseZone.bind(this);
-    this.checkLevel = this.checkLevel.bind(this);
+    this.createGrid = this.createGrid.bind(this);
   }
 
-  ////////// CRAWL! //////////
+  componentDidMount() {
 
-  crawl() {
-
-    console.log('crawl');
-    this.crawling();
-    this.resetHealth();
-    this.checkLevel();
-
-    setTimeout(() => {
-
-      this.reduceHealth(this.state.playerLevel, this.state.damageMultiplier);
-      this.addExp();
-      this.increaseZone();
-      this.crawling();
-    }, 2000)
+    this.createGrid();
   }
 
-  crawling() {
+  createGrid() {
+    let size = this.state.gridSize;
+    let grid = Array(size).fill(0).map(()=>Array(size).fill(0));
 
-    this.setState(() => ({
-      crawling: !this.state.crawling
-    }))
+    this.setState(() => ({ grid  }))
   }
-
-  ////////// HEALTH //////////
-
-  resetHealth() {
-
-    console.log('reset health');
-    this.setState(() => ({
-      health: this.state.maxHealth
-    }))
-  }
-
-  reduceHealth(damage, multiplier) {
-
-    let totalDamage = Math.floor(Math.random() * (damage * multiplier)) + 1;
-
-    console.log('reduce health', totalDamage);
-    this.setState((prevState) => ({
-      health: prevState.health - totalDamage
-    }))
-  }
-
-  increaseMaxHealth() {
-
-    this.setState((prevState) => ({
-      maxHealth: prevState.maxHealth + 2,
-      health: prevState.maxHealth + 2
-    }))
-  }
-
-  ////////// EXP/LEVEL //////////
-
-  checkLevel() {
-
-    let zone = this.state.zone;
-
-    if (zone % 10 === 0) {
-      this.setState((prevState) => ({
-        damageMultiplier: prevState.damageMultiplier + 2
-      }))
-    }
-  }
-
-  addExp() {
-
-    console.log('add exp');
-    this.setState((prevState) => ({
-      exp: prevState.exp + 2
-    }));
-
-    if (this.state.exp >= this.state.maxExp) {
-      this.levelUp()
-    };
-  }
-
-  levelUp() {
-
-    this.setState((prevState) => ({
-      playerLevel: prevState.playerLevel + 1,
-      skillPoints: prevState.skillPoints + 2,
-      exp: 0,
-      gainedLevel: true,
-      maxExp: prevState.maxExp + (prevState.maxExp * 1.2)
-    }))
-  }
-
-  increaseZone() {
-
-    this.setState((prevState) => ({
-      zone: prevState.zone + 1
-    }))
-  }
-
+  
   render() {
+    console.log(this.state.grid);
     return (
-      <div>
-        <h1>{this.state.crawling ? 'Crawling' : null}</h1>
-        <h1>{this.state.zone % 10 === 0 ? 'BOSS FIGHT' : null}</h1>
-        {
-          this.state.health > 0 ?
-
-            <Player
-              playerLevel={this.state.playerLevel}
-              exp={this.state.exp}
-              maxExp={this.state.maxExp}
-              health={this.state.health}
-              skillPoints={this.state.skillPoints}
-              crawl={this.crawl}
-              crawling={this.state.crawling}
-              zone={this.state.zone}
-            />
-
-            :
-
-            <h1>Ded</h1>
-        }
-        <Masteries
-          increaseMaxHealth={this.increaseMaxHealth} />
-      </div>
+      <div></div>
     );
   }
 }
